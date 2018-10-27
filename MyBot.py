179e2c5adac315ba259ce082e174a0527520b8ec
stefank0 <stefankorenberg@hotmail.com>
2018-10-27 11:08:36 +0200

    Initial commit

---
+++ MyBot.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import logging, hlt, time
+from hlt import constants, Direction, Position
+from statistics import median
+from scheduling import Schedule, Assignment
+
+#
+# Idee: definieer hoeveel stappen halite waard is, om te bepalen of er een dropoff moet komen en zo ja waar. Als besloten waar, dan moet deze al beschikbaar zijn als toekomstige dropoff, zodat ships daar alvast naartoe kunnen bewegen (de eerste die er is, maakt de dropoff aan).
+#
+
+returning_to_shipyard = set()
+
+def get_checked_positions(distance, ship):
+    positions = []
+    for i in range(-distance, distance+1):
+        for j in range(-distance, distance+1):
+            if abs(i) + abs(j) > distance:
+                continue
+            position = ship.position + Position(i, j)
+            positions.append(position)
+    return positions
+
+def find_new_halite(ship):
+    halite_amount = 0
+    distance = 0
+    while halite_amount < 0.05 * constants.MAX_HALITE and distance < 10:
+        distance += 1
+        checked_positions = get_checked_positions(distance, ship)
+        destination = max(checked_positions, key=lambda position: game_map[position].halite_amount)
+        halite_amount = game_map[destination].halite_amount
+    return destination
+
+def returning(ship):
+    return (ship.halite_amount > 0.75 * constants.MAX_HALITE) or (ship.id in returning_to_shipyard)
+
+def mining(ship, local_halite):
+    return (local_halite > 0.05 * constants.MAX_HALITE) or (0.2 * local_halite > ship.halite_amount)
+
+def create_schedule():
+    # Preprocessing.
+    for ship in me.get_ships():
+        if ship.halite_amount < 0.25 * constants.MAX_HALITE:
+            returning_to_shipyard.discard(ship.id)
+
+    # Move ships.
+    schedule = Schedule(game_map)
+    for ship in me.get_ships():
+        local_halite = game_map[ship].halite_amount
+        if returning(ship):
+            returning_to_shipyard.add(ship.id)
+            destination = me.shipyard.position
+        elif mining(ship, local_halite):
+            destination = ship.position
+        else:
+            destination = find_new_halite(ship)
+        schedule.assign(ship, destination)
+    return schedule
+
+
+
+
+
+
+
+
+
+
+
+
+
+def add_move_commands(command_queue):
+    """Add movement commands to the command queue."""
+    start = time.time()
+    schedule = create_schedule()
+    end = time.time()
+    logging.info(end-start)
+    start = time.time()
+    command_queue.extend(schedule.to_commands())
+    end = time.time()
+    logging.info(end-start)
+
+def other_players():
+    """Get a list of the other players."""
+    return [player for player in game.players.values() if not player is me]
+
+def number_of_ships(player):
+    """Get the number of ships of a player."""
+    return len(player.get_ships())
+
+def _ship_number_falling_behind():
+    """Return True if our ship number isn't high compared to the others."""
+    ship_numbers = [number_of_ships(player) for player in other_players()]
+    return number_of_ships(me) <= median(ship_numbers)
+
+def want_to_spawn():
+    """Return True if we would like to spawn a new ship."""
+    is_early_game = game.turn_number <= constants.MAX_TURNS/2
+    is_late_game = game.turn_number >= constants.MAX_TURNS*3/4
+    is_mid_game = (not is_early_game) and (not is_late_game)
+    return is_early_game or (is_mid_game and _ship_number_falling_behind())
+
+def can_spawn():
+    """Return True if it is possible to spawn a new ship."""
+    enough_halite = me.halite_amount >= constants.SHIP_COST
+    shipyard_free = not game_map[me.shipyard].is_occupied
+    return enough_halite and shipyard_free
+
+def add_spawn_command(command_queue):
+    """If possible and desirable, add the spawn command."""
+    if can_spawn() and want_to_spawn():
+        command_queue.append(me.shipyard.spawn())
+
+def generate_commands():
+    """Generate a list of commands based on the current game state."""
+    command_queue = []
+    add_move_commands(command_queue)
+    add_spawn_command(command_queue)
+    return command_queue
+
+def mark_safe():
+    """Undo marking that was done in game.update_frame()."""
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            game_map[Position(x, y)].ship = None
+
+
+# Initialize the game.
+game = hlt.Game()
+game.ready("Schildpad")
+
+# Define some globals for convenience.
+me = game.me
+game_map = game.game_map
+
+# Play the game.
+while True:
+    game.update_frame()
+    mark_safe()
+    command_queue = generate_commands()
+    game.end_turn(command_queue)
---
+++ scheduling.py
@@ -0,0 +1,142 @@
+from hlt import Direction, Position
+from scipy.optimize import linear_sum_assignment
+import numpy as np
+
+#
+# Oplossing bij einde spel, wanneer ze elkaar mogen raken op shipyard/dropoffs:
+# - Extra artificial targets maken, zodat shipyards vaker gekozen kunnen worden.
+# - Of de schepen grenzend aan een shipyard ertussenuit pikken en deze direct accepteren.
+#
+#
+#
+
+
+def calc_distances(origin, destination):
+    """Calculates distances in all directions. Incorporates toroid metric."""
+    dy = destination.y - origin.y
+    dx = destination.x - origin.x
+    height = game_map.height
+    width = game_map.width
+    d_south = dy if dy >= 0 else height + dy
+    d_north = height - dy if dy >= 0 else -dy
+    d_east = dx if dx >= 0 else width + dx
+    d_west = width - dx if dx >= 0 else -dx
+    return (d_north, d_south, d_east, d_west)
+
+def effective_directions(origin, destination):
+    """Get a list of effective directions to get closer to the destination."""
+    directions = []
+    (d_north, d_south, d_east, d_west) = calc_distances(origin, destination)
+    if 0 < d_south <= d_north:
+        directions.append(Direction.South)
+    if 0 < d_north <= d_south:
+        directions.append(Direction.North)
+    if 0 < d_west <= d_east:
+        directions.append(Direction.West)
+    if 0 < d_east <= d_west:
+        directions.append(Direction.East)
+    if d_north == d_south == d_east == d_west == 0:
+        directions.append(Direction.Still)
+    return directions
+
+def target(origin, direction):
+    """Calculate the target cell if the ship moves in the given direction."""
+    return game_map[origin.directional_offset(direction)]
+
+def index_to_cell(index):
+    """Map a 1D index to a 2D MapCell."""
+    x = index % game_map.width
+    y = index // game_map.width
+    return game_map[Position(x, y)]
+
+def cell_to_index(cell):
+    """Map a 2D MapCell to a 1D index."""
+    x = cell.position.x
+    y = cell.position.y
+    return x + game_map.width*y
+
+
+class Assignment:
+    """An assignment of a ship to a destination."""
+
+    def __init__(self, ship, destination):
+        self.ship = ship
+        self.destination = destination
+
+    def targets(self):
+        """Get a list of proper target cells for the next move."""
+        origin = self.ship.position
+        directions = effective_directions(origin, self.destination)
+        return [target(origin, direction) for direction in directions]
+
+    def to_command(self, target_cell):
+        """Return command to move its ship to a target cell."""
+        target_cell.mark_unsafe(self.ship)
+
+        if target_cell == game_map[self.ship]:
+            return self.ship.stay_still()
+
+        origin = self.ship.position
+        for direction in Direction.get_all_cardinals():
+            if target_cell == target(origin, direction):
+                return self.ship.move(direction)
+
+
+class Schedule:
+    """Keeps track of Assignments and translates them into a command list."""
+
+    def __init__(self, _game_map):
+        global game_map
+        game_map = _game_map
+        self.assignments = []
+
+    def assign(self, ship, destination):
+        """Assign a ship to a destination."""
+        assignment = Assignment(ship, destination)
+        self.assignments.append(assignment)
+
+    def initial_cost_matrix(self):
+        """Initialize the cost matrix with high costs for every move.
+
+        Note:
+            The rows/columns of the cost matrix represent ships/targets. An
+            element in the cost matrix represents the cost of moving a ship
+            to a target. Some elements represent moves that are not possible
+            in a single turn. However, because these have high costs, they will
+            never be chosen by the algorithm.
+        """
+        n = len(self.assignments)           # Number of assignments/ships.
+        m = game_map.width*game_map.height  # Number of cells/targets.
+        return np.full((n, m), 9999)
+
+    def reduce_stay_still(self, cost_matrix):
+        """Not moving is a reasonable move for all ships."""
+        for i, assignment in enumerate(self.assignments):
+            current_cell = game_map[assignment.ship]
+            j = cell_to_index(current_cell)
+            cost_matrix[i][j] = 1
+
+    def reduce_targets(self, cost_matrix):
+        """The lowest costs are moves in the direction of the destination."""
+        for i, assignment in enumerate(self.assignments):
+            for target in assignment.targets():
+                j = cell_to_index(target)
+                cost_matrix[i][j] = 0
+
+    def create_cost_matrix(self):
+        """"Create a cost matrix for linear_sum_assignment()."""
+        cost_matrix = self.initial_cost_matrix()
+        self.reduce_stay_still(cost_matrix)
+        self.reduce_targets(cost_matrix)
+        return cost_matrix
+
+    def to_commands(self):
+        """Translate the assignments of ships to commands."""
+        commands = []
+        cost_matrix = self.create_cost_matrix()
+        row_ind, col_ind = linear_sum_assignment(cost_matrix)
+        for i, j in zip(row_ind, col_ind):
+            assignment = self.assignments[i]
+            target = index_to_cell(j)
+            commands.append(assignment.to_command(target))
+        return commands
