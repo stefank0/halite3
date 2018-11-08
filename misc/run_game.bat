dbf821a5bff05c752753aa06f77062ee9d49fe2a
jonnekleijer <jonne.kleijer@rhdhv.com>
2018-11-08 17:31:43 +0100

    Add local run files in misc folder

---
+++ misc/run_game.bat
@@ -0,0 +1,1 @@
+halite.exe --replay-directory ../replays/ -vvv --turn-limit 312 --width 32 --height 32 "python ../MyBot.py" "python ../MyBot.py"
---
+++ misc/run_game4.bat
@@ -0,0 +1,1 @@
+halite.exe --replay-directory ../replays/ -n 4 --turn-limit 312 "python ../MyBot.py" "python ../MyBot.py" "python ../MyBot.py" "python ../MyBot.py"
