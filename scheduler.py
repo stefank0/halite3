16d990e62a18ab895dd9d919f16e5173a626cf2b
jonnekleijer <jonne.kleijer@rhdhv.com>
2018-11-19 20:04:09 +0100

    Refactor ship_density

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,4 @@
-import logging, math
+import logging, math, time
 
 from hlt import constants
 from scipy.optimize import linear_sum_assignment
@@ -132,7 +132,7 @@ class Scheduler:
         halites = np.array([ship.halite_amount + self.game_map[ship].halite_amount for ship in ships])
         costs = constants.DROPOFF_COST - halites
         halite_densities = np.array([self.map_data.halite_density[to_index(ship)] for ship in ships])
-        ship_densities = np.array([self.map_data.density_ships[to_index(ship)] for ship in ships])
+        ship_densities = np.array([self.map_data.ship_density[to_index(ship)] for ship in ships])
         dists = self.map_data.distance_dropoffs(ships)
         suitability = (
                 halite_densities *
