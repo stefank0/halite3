4452afd867f9394e87b2829f1de327ff79f3ea76
jonnekleijer <jonne.kleijer@rhdhv.com>
2019-01-16 00:31:30 +0100

    Set up for calibration with the threat back to one

--- misc/calibrator.py
+++ misc/calibrator.py
@@ -290,7 +290,7 @@ class Calibrator:
 @click.option('--param', default='', help='Parameter to be trained.')
 @click.option('--dir_output', help='Folder of previous calibration in case you want to continue a calibration.')
 def main(mapsize, n_player, n_games, n_iter, dir_output, convergence, param):
-    parameters = [param] if param else ['return_distance_factor', 'neighbour_profit_factor', 'return_factor', 'expand_edge_cost']
+    parameters = [param] if param else ["draw_from_shipyard", "expansion_factor", "dropoff_halite_density"]
     calibrator = Calibrator(parameters=parameters,
                             mapsize=int(mapsize), n_player=int(n_player), n_games=int(n_games), n_iter=int(n_iter),
                             convergence=float(convergence), dir_output=dir_output)
